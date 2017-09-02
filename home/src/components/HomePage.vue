e871076428d5e57467070de0c2b26079612f1bcc
wangrunzhuang <wangrunzhuang@piyingke.com>
2017-09-03 00:15:55 +0800

    顶部导航

---
+++ home/src/components/HomePage.vue
@@ -0,0 +1,30 @@
+<template>
+  
+  <!-- 个人主页 -->
+  <div class="home-page">
+
+    <!-- 头部组件 -->
+    <navigation />
+
+  </div>
+
+</template>
+
+<script>
+
+  // 引入头部组件
+  import Navigation from '@/components/Header'
+
+  export default {
+    name: 'home-page',
+    components: {
+      Navigation
+    },
+    data () {
+      return {
+        
+      }
+    }
+  }
+</script>
+
---
+++ test/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    count is {{ $store.state.count }}
+    <button @click="increment">+5</button>
+    <button @click="decrement">-3</button>
+  </div>
+</template>
+
+<script>
+  import { mapGetters, mapActions } from 'vuex'
+
+  export default {
+    computed: mapGetters ([
+
+    ]),
+    methods: mapActions ([
+      'increment',
+      'decrement'
+    ])
+  }
+</script>
