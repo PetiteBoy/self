192ce572f13a3d8e6331b79869191d60c5aa8df2
“wangrunzhuang” <619410008@qq.com>
2017-12-13 12:17:25 +0800

    行内报错

--- home/src/components/Header.vue
+++ home/src/components/Header.vue
@@ -1,57 +1,68 @@
 <template>
-  
+
   <div class="header">
-    
+
     <div class="inner">
 
       <ul class="list">
-        
-        <li class="item" v-for="item, index in nav_list" :data-hover="item">{{item}}</li>
+
+        <li class="item" v-for="(item, index) in nav_list" :key="index" :data-hover="item">{{item}}</li>
       </ul>
     </div>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'header',
-    data () {
-      return {
-        nav_list: ['首页', '个人作品', '个人简历', '关于']
-      }
-    }      
+export default {
+  name: 'header',
+  data() {
+    return {
+      nav_list: ['首页', '个人作品', '个人简历', '关于']
+    }
   }
+}
 </script>
 
 <style lang="stylus" scoped>
-  .header
-    width 100%
-    height 50px
-    line-height 50px 
-    background #03a9f4
-    .inner
-      overflow hidden
-      width 1200px
-      box-sizing border-box
-      margin 0 auto
-      .list
-        width 100%
-        overflow hidden
-        .item
-          width 150px
-          text-align center
-          float left
-          color #fff
-          cursor pointer
-          font-size 16px
-          position relative
-          transition transform .5s
-          font-weight 400
-          &:hover
-            transform translateY(-50px)
-          &::before 
-            position absolute
-            top 50px
-            content attr(data-hover)
-            transform translate3d(0,0,0)  
+.header {
+  width: 100%;
+  height: 50px;
+  line-height: 50px;
+  background: #03a9f4;
+
+  .inner {
+    overflow: hidden;
+    width: 1200px;
+    box-sizing: border-box;
+    margin: 0 auto;
+
+    .list {
+      width: 100%;
+      overflow: hidden;
+
+      .item {
+        width: 150px;
+        text-align: center;
+        float: left;
+        color: #fff;
+        cursor: pointer;
+        font-size: 16px;
+        position: relative;
+        transition: transform 0.5s;
+        font-weight: 400;
+
+        &:hover {
+          transform: translateY(-50px);
+        }
+
+        &::before {
+          position: absolute;
+          top: 50px;
+          content: attr(data-hover);
+          transform: translate3d(0, 0, 0);
+        }
+      }
+    }
+  }
+}
 </style>
